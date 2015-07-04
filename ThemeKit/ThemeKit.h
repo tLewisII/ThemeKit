5bf90cb944d3c6e91780d3529b42539e38166d23
Terry Lewis <terry@ploverproductions.com>
2015-07-04 14:38:46 -0500

    Initial Commit

---
+++ ThemeKit/ThemeKit.h
@@ -0,0 +1,19 @@
+//
+//  ThemeKit.h
+//  ThemeKit
+//
+//  Created by Terry Lewis II on 7/4/15.
+//  Copyright © 2015 Plover Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThemeKit.
+FOUNDATION_EXPORT double ThemeKitVersionNumber;
+
+//! Project version string for ThemeKit.
+FOUNDATION_EXPORT const unsigned char ThemeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThemeKit/PublicHeader.h>
+
+
